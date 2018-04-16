52aac2515bee890a0bff92d24516eda2efcf46c1
josercc <zhanghang@globalegrow.com>
2018-04-16 11:55:15 +0800

    修改显示的名称

---
+++ YBPopupMenu iOS/YBPopupMenu_iOS.h
@@ -0,0 +1,19 @@
+//
+//  YBPopupMenu_iOS.h
+//  YBPopupMenu iOS
+//
+//  Created by 张行 on 2018/4/16.
+//  Copyright © 2018年 LYB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YBPopupMenu_iOS.
+FOUNDATION_EXPORT double YBPopupMenu_iOSVersionNumber;
+
+//! Project version string for YBPopupMenu_iOS.
+FOUNDATION_EXPORT const unsigned char YBPopupMenu_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YBPopupMenu_iOS/PublicHeader.h>
+
+
