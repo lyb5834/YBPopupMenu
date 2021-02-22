b622a604829a240a0c36d2028f24ce87fb2087b8
lyb5834 <lyb5834@126.com>
2021-02-22 12:23:54 +0800

    增加Swiftpackage支持

--- Package.swift
+++ Package.swift
@@ -5,26 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "YBPopupMenu",
+    platforms: [.iOS(.v9)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "YBPopupMenu",
             targets: ["YBPopupMenu"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "YBPopupMenu",
-            path: "Sources/YBPopupMenu",
-            publicHeadersPath : "Sources/YBPopupMenu"
-        ),
-        .testTarget(
-            name: "YBPopupMenuTests",
-            dependencies: ["YBPopupMenu"]),
+            path: "YBPopupMenu",
+            publicHeadersPath : ""
+        )
     ]
 )
